2e228022d0f59129c85b1c2039427fd6d2b57449
umbobabo <umbertobabini@economist.com>
2017-02-01 17:11:59 +0000

    BREAKING CHANGE: create list of links from array, BEM complaiant

--- src/example.js
+++ src/example.js
@@ -1,11 +1,34 @@
 import AdminBar from './index';
 import React from 'react';
 
+const menuLinks = [
+  {
+    label: 'First menu',
+    url: 'http://www.google.com',
+    className: 'firstMenuClassName',
+  },
+  {
+    label: 'Second menu',
+    url: 'http://www.facebook.com',
+    className: 'secondMenuClassName',
+  },
+];
 export default (
-  <AdminBar
-    title="Admin bar"
-    classNamePrefix="admin-bar-wrapper"
-    className="admin-bar-wrapper"
-    editLinkHref="http://www.economist.com"
-  />
+  <div>
+    <h2>Drop down version</h2>
+    <AdminBar
+      title="Admin bar"
+      classNamePrefix="admin-bar"
+      className="admin-bar-wrapper"
+      menuList={menuLinks}
+      mode="dropdown"
+    />
+    <h2>Buttons version</h2>
+    <AdminBar
+      title="Admin bar"
+      classNamePrefix="admin-bar"
+      className="admin-bar-wrapper"
+      menuList={menuLinks}
+    />
+  </div>
 );
--- src/index.js
+++ src/index.js
@@ -2,43 +2,87 @@ import React from 'react';
 import Button from '@economist/component-link-button';
 import BarWrapper from '@economist/component-bar-wrapper';
 
-export default function AdminBar({ className, title, editLinkHref, renderEditLink }) {
-  const classNamesList = [ 'admin-bar' ];
+export default function AdminBar({ classNamePrefix = 'admin-bar', className, title, menuList, mode }) {
+  const classNamesList = [ classNamePrefix ];
   const children = [];
   if (className) {
     classNamesList.push(className);
   }
-
   const barWrapperClassName = classNamesList.join(' ');
-  if (title) {
-    children.push((
-      <span key="title" className="admin-bar__title">{title}</span>
-    ));
+  const adminTitle = title ? (<span key="title" className={`${ classNamePrefix }__title`}>{title}</span>) : null;
+  function createButtonList({ label, url, className, key, target = '_blank' }) {
+    const additionalClassname = (className) ? ` ${ className }` : '';
+    const props = {
+      href: url,
+      children: label,
+      className: `${ classNamePrefix }__edit-link${ additionalClassname }`,
+      key,
+      target,
+    };
+    return (<Button {...props} />);
   }
-  const editLinkDefaultProps = {
-    className: 'admin-bar__edit-link',
-    href: editLinkHref,
-    target: '_blank',
-    children: 'Edit this article',
-    key: 'editLink',
-  };
-  const editLinkButton = renderEditLink ? renderEditLink() : (
-    <Button {...editLinkDefaultProps} />
-  );
-  children.push(editLinkButton);
+
+  function createOptionList({ label, url, className, key }) {
+    const additionalClassname = (className) ? ` ${ className }` : '';
+    const props = {
+      value: url,
+      children: label,
+      className: `${ classNamePrefix }__option${ additionalClassname }`,
+      key,
+    };
+    return (<option {...props} />);
+  }
+
+  let renderChild = null;
+  switch (mode) {
+    case 'dropdown':
+      renderChild = createOptionList;
+      break;
+    default:
+      renderChild = createButtonList;
+  }
+
+  menuList.forEach((menuItem, index) => {
+    menuItem.key = index;
+    children.push(renderChild(menuItem));
+  });
+
+  function handleChange(event) {
+    // Ignore empty values.
+    if (event.target.value) {
+      window.open(event.target.value, '_blank');
+    }
+  }
+
+  let adminElements = children;
+  if (mode === 'dropdown') {
+    adminElements = (
+      <select className={`${ classNamePrefix }__select`} onChange={handleChange}>
+        <option value="">Please select an option</option>
+        {children}
+      </select>
+    );
+  }
+
   return (
     <BarWrapper className={barWrapperClassName} close={false}>
-      {children}
+      {adminTitle}
+      {adminElements}
     </BarWrapper>
   );
 }
 
 if (process.env.NODE_ENV !== 'production') {
   AdminBar.propTypes = {
+    classNamePrefix: React.PropTypes.string,
     className: React.PropTypes.string,
     children: React.PropTypes.node,
-    renderEditLink: React.PropTypes.func,
-    editLinkHref: React.PropTypes.string,
     title: React.PropTypes.string,
+    menuList: React.PropTypes.arrayOf(React.PropTypes.shape({
+      label: React.PropTypes.string,
+      url: React.PropTypes.string,
+      className: React.PropTypes.string,
+    })).isRequired,
+    mode: React.PropTypes.oneOf([ 'dropdown' ]),
   };
 }
